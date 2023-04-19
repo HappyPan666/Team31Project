413b4cd69991eb96d34dfda0d1427a6efc1127ea
Zhangjin-666 <Patrick1998pzj@gmail.com>
2023-04-18 21:56:09 -0700

    design and write login page.xml

--- app/src/main/java/edu/northeastern/team31project/final_login.java
+++ app/src/main/java/edu/northeastern/team31project/final_login.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class finalLogin extends AppCompatActivity {
+public class final_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
